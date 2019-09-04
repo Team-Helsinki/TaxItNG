670e8906aa9610413fe40dfa188b41fc374a2188
samuel Temitope <stemitope370@gmail.com>
2019-09-04 19:50:39 +0100

    user profile added

---
+++ profile_page.js
@@ -0,0 +1,44 @@
+// const submit = document.querySelector('form_submit');
+
+// function validation() {
+//     var name = document.querySelector('name').value;
+//     var email = document.querySelector('email').value;
+
+//     if (name === '' || email === '') {
+//         alert('Please fill all fields...!!!');
+//         return false;
+//     } else {
+//         return true;
+//     }
+// }
+
+
+
+
+
+
+// var modal = document.querySelector(".modal");
+// var submit = document.querySelector(".trigger");
+// var closeButton = document.querySelector(".close-button");
+
+// function toggleModal() {
+//     modal.classList.toggle("show-modal");
+// }
+
+// function windowOnClick(event) {
+//     if (event.target === modal) {
+//         toggleModal();
+//     }
+// }
+
+// trigger.addEventListener("click", toggleModal);
+// closeButton.addEventListener("click", toggleModal);
+// window.addEventListener("click", windowOnClick);
+
+
+// const submit = document.querySelector('form_submit');
+// const form = document.querySelector('form');
+
+// function submitForm{
+//     form.onsubmit.alert('Success!!!');
+// }
